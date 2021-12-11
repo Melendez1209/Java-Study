c985c8dec07d3e1d88a229c46bdd94ef7b2625ea
Mark Melendez <suge1209@163.com>
2021-12-11 22:47:24 +0800

    2021-12-11 22:47:20

---
+++ 入门/笔记/1.HelloWorld/src/first.java
@@ -0,0 +1,8 @@
+//定义一个类
+//类名与文件名保持一致，首字母必输大写
+public class first {
+    //    main函数
+    public static void main(String[] args) {
+        System.out.println("Hello World     你好世界");//输出语句
+    }
+}
---
+++ 入门/笔记/2.注释/src/note.java
@@ -0,0 +1,16 @@
+/**
+ * 这是一个文档注释
+ *
+ * @author Melendez
+ * @version 1.0
+ * @coding utf-8
+ * @time 2021-12-11 15:09:23
+ */
+public class note {
+    public static void main(String[] args) {//这是一个单行注释
+    /*这
+    是一个多行注释
+     */
+    }
+
+}
