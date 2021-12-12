ab30d257f433bd6e9e05624be97c235ca419efce
Mark Melendez <suge1209@163.com>
2021-12-12 15:19:16 +0800

    Unit

---
+++ 工具/模板.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+/**
+ * @title:${NAME}.java
+ * @description:
+ * @author:Mark Melendez
+ * @date ${DATE}${TIME}
+ * @Version
+ */
+#parse("File Header.java")
+public class ${NAME} {
+        }
