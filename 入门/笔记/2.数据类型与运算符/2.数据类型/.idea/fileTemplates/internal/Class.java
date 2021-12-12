2d53e4724ae80cfc63161c787526633ff4fc7290
Mark Melendez <suge1209@163.com>
2021-12-12 20:57:48 +0800

    数据类型

---
+++ 入门/笔记/2.数据类型与运算符/2.数据类型/.idea/fileTemplates/internal/Class.java
@@ -0,0 +1,15 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+/**
+ * @title:${NAME}.java
+ * @description:
+ * @author:Mark Melendez
+ * @date ${DATE}${TIME}
+ * @Version
+ */
+#parse("File Header.java")
+public class ${NAME} {
+    public static void mian(String[] args) {
+
+        
+    }
+        }
---
+++ 入门/笔记/2.数据类型与运算符/2.数据类型/src/Int_1.java
@@ -0,0 +1,9 @@
+public class Int_1 {
+    public static void mian(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        System.out.println(c);
+
+    }
+}
