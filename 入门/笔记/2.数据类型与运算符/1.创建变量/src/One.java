8b62db9f5d3c6b91255790a16408f2bab7589b65
Mark Melendez <suge1209@163.com>
2021-12-12 18:38:44 +0800

    数据变量

---
+++ 入门/笔记/2.数据类型与运算符/1.创建变量/src/One.java
@@ -0,0 +1,11 @@
+public class One {
+
+    public static void main(String[] args) {
+        //声明变量
+        int age;
+        //赋值
+        age = 1;
+        //使用变量
+        System.out.println("年龄：" + age);
+    }
+}
---
+++ 入门/笔记/2.数据类型与运算符/1.创建变量/src/Two.java
@@ -0,0 +1,13 @@
+public class Two {
+    public static void main(String[] args) {
+        //声明并赋值
+        int age = 1;
+        //使用变量
+        System.out.println("年龄：" + age);
+        /*2 . 驼峰式命名法：
+
+        又叫小驼峰式命名法。
+        第一个单词首字母小写，后面其他单词首字母大写。(或替换为下划线)*/
+
+    }
+}
